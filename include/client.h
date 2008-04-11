bbce52f18d2afe322cd49871d1768ce7b5d91aca
William Pitcock <nenolod@atheme.org>
2008-04-11 10:23:47 -0500

    TS_MIN is 6 now.

--- include/client.h
+++ include/client.h
@@ -374,12 +374,7 @@ struct exit_client_hook
  * ts stuff
  */
 #define TS_CURRENT	6
-
-#ifdef TS6_ONLY
 #define TS_MIN          6
-#else
-#define TS_MIN          3
-#endif
 
 #define TS_DOESTS       0x10000000
 #define DoesTS(x)       ((x)->tsinfo & TS_DOESTS)
