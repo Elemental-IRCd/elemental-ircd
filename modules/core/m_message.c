6afd4b916f936fcab6ab89a5f9887e9d2cc365ce
B.Greenham <taros34@hotmail.com>
2010-02-23 00:49:24 -0500

    Add chmode +T, which blocks notices.

--- modules/core/m_message.c
+++ modules/core/m_message.c
@@ -513,6 +513,12 @@ msg_channel(int p_or_n, const char *command,
 		if(result == CAN_SEND_OPV ||
 		   !flood_attack_channel(p_or_n, source_p, chptr, chptr->chname))
 		{
+			if (p_or_n != PRIVMSG && chptr->mode.mode & MODE_NONOTICE)
+			{
+				sendto_one_numeric(source_p, ERR_CANNOTSENDTOCHAN,
+						form_str(ERR_CANNOTSENDTOCHAN), chptr->chname);
+				return;
+			}
 			if (p_or_n != NOTICE && *text == '\001' &&
 					strncasecmp(text + 1, "ACTION", 6))
 			{
