473d0842db706ff65bb1d959d974751904bc8621
B.Greenham <taros34@hotmail.com>
2010-03-17 02:19:18 -0400

    Fix some compiler warnings that I generated by accidentally copying something twice.

--- include/channel.h
+++ include/channel.h
@@ -299,10 +299,6 @@ extern void do_join_0(struct Client *client_p, struct Client *source_p);
 extern int check_channel_name_loc(struct Client *source_p, const char *name);
 
 extern struct Metadata *channel_metadata_add(struct Channel *target, const char *name, const char *value, int propegate);
-extern void channel_metadata_delete(struct Channel *target, const char *name, int propegate);
-extern struct Metadata *channel_metadata_find(struct Channel *target, const char *name);
-
-extern struct Metadata *channel_metadata_add(struct Channel *target, const char *name, const char *value, int propegate);
 extern struct Metadata *channel_metadata_time_add(struct Channel *target, const char *name, time_t timevalue, const char *value);
 extern void channel_metadata_delete(struct Channel *target, const char *name, int propegate);
 extern struct Metadata *channel_metadata_find(struct Channel *target, const char *name);
