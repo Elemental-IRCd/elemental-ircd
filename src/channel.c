4b631c0586b621747876267e8a16e076904f51de
JD Horelick <jdhore1@gmail.com>
2010-12-15 00:02:47 -0500

    Send ERR_TOOMANYCHANNELS for each channel join that fails due to channel limits. The reason why we do this is because some clients are dependent on receiving a numeric for every channel join failure, even due to this limit where it can be assumed that subsequent joins failed.

--- src/channel.c
+++ src/channel.c
@@ -1900,7 +1900,7 @@ void user_join(struct Client * client_p, struct Client * source_p, const char * 
 		{
 			sendto_one(source_p, form_str(ERR_TOOMANYCHANNELS),
 				   me.name, source_p->name, name);
-			return;
+			continue;
 		}
 
 		if(chptr == NULL)	/* If I already have a chptr, no point doing this */
