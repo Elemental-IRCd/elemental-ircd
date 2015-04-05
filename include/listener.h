167e70073a139183d93dc9cfadfd5dd1b4124f20
Andrew Cook <ariscop@gmail.com>
2015-04-06 09:51:43 +1000

    Tabs to spaces in listener.h

--- include/listener.h
+++ include/listener.h
@@ -31,16 +31,16 @@
 struct Client;
 
 struct Listener {
-    struct Listener *next;	/* list node pointer */
-    const char *name;	/* listener name */
-    rb_fde_t *F;		/* file descriptor */
-    int ref_count;		/* number of connection references */
-    int active;		/* current state of listener */
-    int ssl;		/* ssl listener */
-    int defer_accept;       /* use TCP_DEFER_ACCEPT */
+    struct Listener *next;   /* list node pointer */
+    const char *name;        /* listener name */
+    rb_fde_t *F;             /* file descriptor */
+    int ref_count;           /* number of connection references */
+    int active;              /* current state of listener */
+    int ssl;                 /* ssl listener */
+    int defer_accept;        /* use TCP_DEFER_ACCEPT */
     struct rb_sockaddr_storage addr;
     struct DNSQuery *dns_query;
-    char vhost[HOSTLEN + 1];	/* virtual name of listener */
+    char vhost[HOSTLEN + 1]; /* virtual name of listener */
 };
 
 extern void add_listener(int port, const char *vaddr_ip, int family, int ssl, int defer_accept);
