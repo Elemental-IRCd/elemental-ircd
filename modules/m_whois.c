c9102645ff394e44c8747f3222c4ec387ce5c2a1
AppleDash <derp@derpy.fr>
2015-05-17 16:26:33 -0400

    m_whois: Fix bug where non-webclient users had webclient whois reply and webclient users did not. Closes #69

--- modules/m_whois.c
+++ modules/m_whois.c
@@ -314,7 +314,7 @@ single_whois(struct Client *source_p, struct Client *target_p, int operspy)
         sendto_one_numeric(source_p, RPL_WHOISSECURE, form_str(RPL_WHOISSECURE),
                            target_p->name);
 
-    if(!(target_p->umodes & UMODE_WEBCLIENT)) {
+    if(target_p->umodes & UMODE_WEBCLIENT) {
         sendto_one_numeric(source_p, RPL_WHOISWEBIRC, form_str(RPL_WHOISWEBIRC),
                            target_p->name);
     }
