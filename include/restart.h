b20885c253ded2a654ab75961f7ecfc423a59ab9
Andrew Cook <ariscop@gmail.com>
2015-07-02 14:30:12 +1000

    Mark server_rebot and restart as noreturn

--- include/restart.h
+++ include/restart.h
@@ -26,7 +26,7 @@
 #ifndef INCLUDED_restart_h
 #define INCLUDED_restart_h
 
-void restart(const char *);
-void server_reboot(void);
+void restart(const char *) __noreturn;
+void server_reboot(void) __noreturn;
 
 #endif
