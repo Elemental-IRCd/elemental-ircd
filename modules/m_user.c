9e5c8630e46d985a0cfe1bdf1834d937976d1756
Andrew Cook <ariscop@gmail.com>
2015-07-17 11:54:20 +1000

    Remove unused variable

--- modules/m_user.c
+++ modules/m_user.c
@@ -81,12 +81,10 @@ static int
 do_local_user(struct Client *client_p, struct Client *source_p,
               const char *username, const char *realname)
 {
-    struct User *user;
-
-    s_assert(NULL != source_p);
+    s_assert(source_p != NULL);
     s_assert(source_p->username != username);
 
-    user = make_user(source_p);
+    make_user(source_p);
 
     if (!(source_p->flags & FLAGS_SENTUSER)) {
         lookup_blacklists(source_p);
