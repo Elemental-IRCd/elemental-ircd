15a12dd5cb2209337101c9a86ecb461e08728719
Andrew Cook <ariscop@gmail.com>
2015-08-24 16:37:54 +1000

    Collect logs on travis builds
    Also changes startall.sh to work from any dir

--- testsuite/startall.sh
+++ testsuite/startall.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+cd `dirname $0`
 testdir=`pwd`
 prefix=`sed -n -e 's/^#define IRCD_PREFIX "\(.*\)"/\1/p' "$testdir/../include/setup.h"`
 [ -d $prefix ] || { echo Unable to find installation prefix; exit 1; }
 
-$prefix/bin/ircd -configfile $testdir/ircd.conf.1 -pidfile $testdir/ircd.pid.1 -logfile $testdir/ircd.log.1 &
-$prefix/bin/ircd -configfile $testdir/ircd.conf.2 -pidfile $testdir/ircd.pid.2 -logfile $testdir/ircd.log.2 &
-$prefix/bin/ircd -configfile $testdir/ircd.conf.3 -pidfile $testdir/ircd.pid.3 -logfile $testdir/ircd.log.3 &
+$prefix/bin/ircd -configfile $testdir/ircd.conf.1 -pidfile $testdir/ircd.pid.1 -logfile logs/ircd.log.1 &
+$prefix/bin/ircd -configfile $testdir/ircd.conf.2 -pidfile $testdir/ircd.pid.2 -logfile logs/ircd.log.2 &
+$prefix/bin/ircd -configfile $testdir/ircd.conf.3 -pidfile $testdir/ircd.pid.3 -logfile logs/ircd.log.3 &
