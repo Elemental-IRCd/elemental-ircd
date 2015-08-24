ac89941247b3bd93c30bbe940652cb3e49bd03f3
Andrew Cook <ariscop@gmail.com>
2015-08-24 18:47:02 +1000

    Log nameservers as they're added
    Useful if tests fail due to dns timeouts

--- src/reslib.c
+++ src/reslib.c
@@ -232,6 +232,8 @@ add_nameserver(const char *arg)
         return;
     }
 
+    ilog (L_MAIN, "Using nameserver: %s", arg);
+
     memset(&hints, 0, sizeof(hints));
     hints.ai_family   = PF_UNSPEC;
     hints.ai_socktype = SOCK_DGRAM;
