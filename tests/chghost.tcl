58e21c87476fa8b549cbde8380c989f948920190
Andrew Cook <ariscop@gmail.com>
2015-09-11 17:40:26 +1000

    Use snit for OO instead of TclOO
    This is (far as i can tell) implemented in pure tcl as part of
    tcllib, so it won't require the messy buggery in .travis.yml

--- tests/chghost.tcl
+++ tests/chghost.tcl
@@ -4,29 +4,30 @@ source lib/lib.tcl
 
 begin test chghost {Check chghost}
 
-set op [client new hub]
+client op   hub
+
 oper
 
-set hub  [client new hub]
-set leaf [client new leaf1]
+client hub  hub
+client leaf leaf1
 
-$op   join_channel #chghost
-$hub  join_channel #chghost
-$leaf join_channel #chghost
+op   join_channel #chghost
+hub  join_channel #chghost
+leaf join_channel #chghost
 
 # wait for joins to propagate
 sleep 3
 
-$op send_cmd CHGHOST [$hub nick] chghost.test
+op send_cmd CHGHOST [hub nick] chghost.test
 
-$op   expect "QUIT :Changing host"
-$leaf expect "QUIT :Changing host"
+op   expect "QUIT :Changing host"
+leaf expect "QUIT :Changing host"
 
-$op   expect "@chghost.test JOIN #chghost"
-$leaf expect "@chghost.test JOIN #chghost"
+op   expect "@chghost.test JOIN #chghost"
+leaf expect "@chghost.test JOIN #chghost"
 
-$hub  expect "chghost.test :is now your hidden host"
+hub  expect "chghost.test :is now your hidden host"
 
-$op   quit
-$hub  quit
-$leaf quit
+op   quit
+hub  quit
+leaf quit
