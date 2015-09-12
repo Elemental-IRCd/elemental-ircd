c6c5aba6e9fa78781d47c1bf3540d887e90ddd87
Andrew Cook <ariscop@gmail.com>
2015-09-13 07:28:37 +1000

    fix umode +g test

--- tests/umodeg.tcl
+++ tests/umodeg.tcl
@@ -6,13 +6,13 @@ client umodegtarget hub
 client umodegsender hub
 
 set message {This message is sent unencumbered}
-umodegsender send_cmd PRIVMSG umodegtarget $message
+umodegsender send_cmd PRIVMSG [umodegtarget nick] $message
 umodegtarget expect  ":$message"
 
-umodegtarget send_cmd MODE umodegtarget +g
+umodegtarget send_cmd MODE [umodegtarget nick] +g
 
 set message {This message will be filtered by +g}
-umodegsender send_cmd PRIVMSG umodegtarget $message
+umodegsender send_cmd PRIVMSG [umodegtarget nick] $message
 umodegtarget expect "is messaging you"
 umodegtarget expect "/ACCEPT"
 umodegsender expect "has been informed that you messaged them."
