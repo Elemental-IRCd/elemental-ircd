9659127b6cab798fac3dfc931b423bbb52e07a53
Andrew Cook <ariscop@gmail.com>
2015-09-13 19:12:22 +1000

    Use the other multi-line syntax
    Like how it looks

--- tests/chghost.tcl
+++ tests/chghost.tcl
@@ -8,11 +8,14 @@ client oper
 client target
 client observer
 
-oper     >> CHGHOST [target nick] chghost.test
-         << QUIT {Changing host}
-         >> "[target nick]!*@chghost.test" JOIN $test_channel
+oper :
+ >> CHGHOST [target nick] chghost.test
+ << QUIT {Changing host}
+ >> "[target nick]!*@chghost.test" JOIN $test_channel
 
-observer << QUIT {Changing host}
-         >> "[target nick]!*@chghost.test" JOIN $test_channel
+observer :
+ << QUIT {Changing host}
+ >> "[target nick]!*@chghost.test" JOIN $test_channel
 
-target   << $RPL_HOSTHIDDEN [target nick] chghost.test "*is now your hidden host*"
+target :
+ << $RPL_HOSTHIDDEN [target nick] chghost.test "*is now your hidden host*"
