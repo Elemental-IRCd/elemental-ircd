91fa625b81041bedfc3b11afb8956244206967cf
Andrew Cook <ariscop@gmail.com>
2015-09-19 15:10:55 +1000

    Add test for chghost cap
    And fix the test, it was sending the join message, not checking
    for it
    >_>

--- tests/chghost.tcl
+++ tests/chghost.tcl
@@ -7,15 +7,21 @@ client oper
 
 client target
 client observer
+client observerv3 -caps {chghost}
 
 oper :
  >> CHGHOST [target nick] chghost.test
  << QUIT {Changing host}
- >> "[target nick]!*@chghost.test" JOIN $test_channel
+ << ":[target nick]!*@chghost.test" JOIN $test_channel
 
 observer :
  << QUIT {Changing host}
- >> "[target nick]!*@chghost.test" JOIN $test_channel
+ << ":[target nick]!*@chghost.test" JOIN $test_channel
+
+observerv3 :
+if {[have chghost]} {
+ << ":[target nick]!*@*" CHGHOST * chghost.test
+}
 
 target :
  << $RPL_HOSTHIDDEN [target nick] chghost.test "*is now your hidden host*"
