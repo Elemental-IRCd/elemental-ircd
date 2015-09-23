f8171e23215c1008467af8b9f13037c5dcdea3de
Christine Dodrill <xena@yolo-swag.com>
2015-09-23 01:14:42 -0700

    tests/omode: try this instead?

--- tests/omode.tcl
+++ tests/omode.tcl
@@ -5,13 +5,16 @@ set test_channel #omode
 client oper
 
 oper :
+    # set up m_omode.so
     >> OPER god testsuite
     >> MODLOAD extensions/m_omode.so
-    << omode.so
+    << NOTICE * "*** Notice -- Module m_omode.so \[version: \$Revision\$; MAPI version: 1\] loaded at"
 
     >> MODE [oper nick] +p
-    << MODE
+    << MODE [oper nick] +p
     >> MODE $test_channel +h [oper nick]
-    << MODE
+    << MODE $test_channel +h [oper nick]
     >> OMODE $test_channel +y [oper nick]
-    << MODE
+    << MODE $test_channel +y [oper nick]
+
+    >> MODUNLOAD m_omode.so
