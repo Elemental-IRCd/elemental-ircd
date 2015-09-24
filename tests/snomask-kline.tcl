bbe3042da4e03f533e2bd78432345778c8c710ad
Christine Dodrill <xena@yolo-swag.com>
2015-09-24 01:20:12 -0700

    Add test for kline snomask
    tests 5ffdbe7525ab37bc57f8e7c76ba28c6fa537778a

---
+++ tests/snomask-kline.tcl
@@ -0,0 +1,32 @@
+begin {snomask for klines}
+
+client god
+    oper god
+    >> MODE [god nick] +s +K
+
+    # Kline bansworth
+    >> KLINE 5 ~bansworth@* ON * :Test
+    << NOTICE
+
+# ensure bansworth is banned (shocker!)
+client klined -user bansworth
+    << ERR_YOUREBANNEDCREEP
+    << ERROR {*Banned*}
+
+god :
+    << NOTICE * {*** Notice -- K-line: bansworth@127.0.0.1 *}
+    >> UNKLINE ~bansworth@*
+    << NOTICE * {*** * has removed the temporary K-Line *}
+
+    # Now test an X-Line
+    >> XLINE 5 eggdrop?bot ON * :no bots
+    << NOTICE * {*** * added temporary 5 min. X-Line for *}
+
+# ensure the xlined gecos doesn't work with the right error
+client xlined -gecos "eggdrop bot"
+    << ERROR {*Bad user info*}
+
+god :
+    << NOTICE * {*** X-line*}
+    >> UNXLINE eggdrop?bot ON *
+    << NOTICE * {X-Line for * is removed}
