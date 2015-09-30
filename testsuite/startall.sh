e0af8f38daeeb90c4dc85447cc8cad028fa7e4e1
Andrew Cook <ariscop@gmail.com>
2015-09-30 15:10:04 +1000

    Sleep between starting ircd's

--- testsuite/startall.sh
+++ testsuite/startall.sh
@@ -16,7 +16,9 @@ cp common.conf $prefix/
 
 echo "Starting ircd's"
 
-$prefix/bin/ircd -configfile $testdir/ircd.conf.1 -pidfile $testdir/ircd.pid.1 -logfile logs/ircd.log.1 &
-$prefix/bin/ircd -configfile $testdir/ircd.conf.2 -pidfile $testdir/ircd.pid.2 -logfile logs/ircd.log.2 &
-$prefix/bin/ircd -configfile $testdir/ircd.conf.3 -pidfile $testdir/ircd.pid.3 -logfile logs/ircd.log.3 &
+$prefix/bin/ircd -configfile $testdir/ircd.conf.1 -pidfile $testdir/ircd.pid.1 -logfile logs/ircd.log.1
+sleep 1
+$prefix/bin/ircd -configfile $testdir/ircd.conf.2 -pidfile $testdir/ircd.pid.2 -logfile logs/ircd.log.2
+sleep 1
+$prefix/bin/ircd -configfile $testdir/ircd.conf.3 -pidfile $testdir/ircd.pid.3 -logfile logs/ircd.log.3
 sleep 5 # let things get all situated
