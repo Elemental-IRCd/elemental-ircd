746b8d9d01747ee3efd7d88aff6c581a0490831b
Christine Dodrill <xena@yolo-swag.com>
2015-10-09 20:56:05 -0700

    Move KNOCK ISUPPORT into m_knock.so
    Ref #179

--- src/supported.c
+++ src/supported.c
@@ -328,7 +328,6 @@ init_isupport(void)
     add_isupport("MAXLIST", isupport_maxlist, NULL);
     add_isupport("MODES", isupport_intptr, &maxmodes);
     add_isupport("NETWORK", isupport_stringptr, &ServerInfo.network_name);
-    add_isupport("KNOCK", isupport_boolean, &ConfigChannel.use_knock);
     add_isupport("STATUSMSG", isupport_string, "~!@%+");
     add_isupport("CALLERID", isupport_string, "g");
     add_isupport("CASEMAPPING", isupport_string, "rfc1459");
