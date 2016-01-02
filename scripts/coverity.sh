859dbb19f04e9c08d59c48dd73f1edb4c94b673b
Andrew Cook <ariscop@gmail.com>
2016-01-02 15:07:32 +1100

    Rig up coverity using our own script
    Avoids duplicating build commands, and doesn't download
    unless we can actually upload a scan
    
    Cleans up how coverage is done also

--- scripts/coverity.sh
+++ scripts/coverity.sh
@@ -1,14 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
-
-# Environment check
-echo -e "\033[33;1mNote: COVERITY_SCAN_PROJECT_NAME and COVERITY_SCAN_TOKEN are available on Project Settings page on scan.coverity.com\033[0m"
-[ -z "$COVERITY_SCAN_PROJECT_NAME" ] && echo "ERROR: COVERITY_SCAN_PROJECT_NAME must be set" && exit 1
-[ -z "$COVERITY_SCAN_NOTIFICATION_EMAIL" ] && echo "ERROR: COVERITY_SCAN_NOTIFICATION_EMAIL must be set" && exit 1
-[ -z "$COVERITY_SCAN_BRANCH_PATTERN" ] && echo "ERROR: COVERITY_SCAN_BRANCH_PATTERN must be set" && exit 1
-[ -z "$COVERITY_SCAN_BUILD_COMMAND" ] && echo "ERROR: COVERITY_SCAN_BUILD_COMMAND must be set" && exit 1
-[ -z "$COVERITY_SCAN_TOKEN" ] && echo "ERROR: COVERITY_SCAN_TOKEN must be set" && exit 1
 
 PLATFORM=`uname`
 TOOL_ARCHIVE=/tmp/cov-analysis-${PLATFORM}.tgz
@@ -16,84 +8,98 @@ TOOL_URL=https://scan.coverity.com/download/${PLATFORM}
 TOOL_BASE=/tmp/coverity-scan-analysis
 UPLOAD_URL="https://scan.coverity.com/builds"
 SCAN_URL="https://scan.coverity.com"
+RESULTS_DIR="cov-int"
+PATH="${TOOL_BASE}/bin/:${PATH}"
 
-# Do not run on pull requests
-if [ "${TRAVIS_PULL_REQUEST}" = "true" ]; then
-  echo -e "\033[33;1mINFO: Skipping Coverity Analysis: branch is a pull request.\033[0m"
-  exit 0
-fi
+MODE=
+while [ -n "$1" ]; do
+  case "$1" in
+    -d | --download )
+        MODE=download; shift ;;
+    -u | --upload )
+        MODE=upload; shift ;;
+    --token )
+        COVERITY_SCAN_TOKEN="$2"; shift 2 ;;
+    --name )
+        COVERITY_SCAN_PROJECT_NAME="$2"; shift 2 ;;
+    -b | --build-command )
+        MODE=build ; shift ;;
+    * )
+        echo "Unknown argument \"$1\""; exit 1 ;;
+  esac
+done
 
-# Verify this branch should run
-IS_COVERITY_SCAN_BRANCH=`ruby -e "puts '${TRAVIS_BRANCH}' =~ /\\A$COVERITY_SCAN_BRANCH_PATTERN\\z/ ? 1 : 0"`
-if [ "$IS_COVERITY_SCAN_BRANCH" = "1" ]; then
-  echo -e "\033[33;1mCoverity Scan configured to run on branch ${TRAVIS_BRANCH}\033[0m"
-else
-  echo -e "\033[33;1mCoverity Scan NOT configured to run on branch ${TRAVIS_BRANCH}\033[0m"
-  exit 1
-fi
+# Environment check
+[ -z "$COVERITY_SCAN_PROJECT_NAME" ] && echo "ERROR: COVERITY_SCAN_PROJECT_NAME must be set" && exit 1
+[ -z "$COVERITY_SCAN_TOKEN" ] && echo "ERROR: COVERITY_SCAN_TOKEN must be set" && exit 1
+[ -z "$COVERITY_SCAN_NOTIFICATION_EMAIL" ] && echo "ERROR: COVERITY_SCAN_NOTIFICATION_EMAIL must be set" && exit 1
 
 # Verify upload is permitted
-AUTH_RES=`curl -s --form project="$COVERITY_SCAN_PROJECT_NAME" --form token="$COVERITY_SCAN_TOKEN" $SCAN_URL/api/upload_permitted`
-if [ "$AUTH_RES" = "Access denied" ]; then
-  echo -e "\033[33;1mCoverity Scan API access denied. Check COVERITY_SCAN_PROJECT_NAME and COVERITY_SCAN_TOKEN.\033[0m"
-  exit 1
-else
-  AUTH=`echo $AUTH_RES | ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['upload_permitted']"`
-  if [ "$AUTH" = "true" ]; then
-    echo -e "\033[33;1mCoverity Scan analysis authorized per quota.\033[0m"
+function cov_check_permitted {
+  AUTH_RES=`curl -s --form project="$COVERITY_SCAN_PROJECT_NAME" --form token="$COVERITY_SCAN_TOKEN" $SCAN_URL/api/upload_permitted`
+  if [ "$AUTH_RES" = "Access denied" ]; then
+    echo -e "\033[33;1mCoverity Scan API access denied. Check COVERITY_SCAN_PROJECT_NAME and COVERITY_SCAN_TOKEN.\033[0m"
+    exit 1
   else
-    WHEN=`echo $AUTH_RES | ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['next_upload_permitted_at']"`
-    echo -e "\033[33;1mCoverity Scan analysis NOT authorized until $WHEN.\033[0m"
-    exit 0
+    AUTH=`echo $AUTH_RES | ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['upload_permitted']"`
+    if [ "$AUTH" = "true" ]; then
+      echo -e "\033[33;1mCoverity Scan analysis authorized per quota.\033[0m"
+    else
+      WHEN=`echo $AUTH_RES | ruby -e "require 'rubygems'; require 'json'; puts JSON[STDIN.read]['next_upload_permitted_at']"`
+      echo -e "\033[33;1mCoverity Scan analysis NOT authorized until $WHEN.\033[0m"
+      exit 0
+    fi
   fi
-fi
+}
 
-if [ ! -d $TOOL_BASE ]; then
-  # Download Coverity Scan Analysis Tool
-  if [ ! -e $TOOL_ARCHIVE ]; then
-    echo -e "\033[33;1mDownloading Coverity Scan Analysis Tool...\033[0m"
-    wget -nv -O $TOOL_ARCHIVE $TOOL_URL --post-data "project=$COVERITY_SCAN_PROJECT_NAME&token=$COVERITY_SCAN_TOKEN"
-  fi
+# Download Coverity Scan Analysis Tool
+function cov_download {
+  mkdir -p $TOOL_BASE
+  pushd $TOOL_BASE
+
+  echo -e "\033[33;1mDownloading Coverity Scan Analysis Tool...\033[0m"
+  wget -nv -O $TOOL_ARCHIVE $TOOL_URL --post-data "project=$COVERITY_SCAN_PROJECT_NAME&token=$COVERITY_SCAN_TOKEN"
 
   # Extract Coverity Scan Analysis Tool
   echo -e "\033[33;1mExtracting Coverity Scan Analysis Tool...\033[0m"
-  mkdir -p $TOOL_BASE
-  pushd $TOOL_BASE
-  tar xzf $TOOL_ARCHIVE
+
+  tar -xz --strip 1 -f $TOOL_ARCHIVE
   popd
-fi
+}
 
-TOOL_DIR=`find $TOOL_BASE -type d -name 'cov-analysis*'`
-export PATH=$TOOL_DIR/bin:$PATH
+# Upload result dir
+function cov_upload {
+    cov-import-scm --dir $1 --scm git --log $RESULTS_DIR/scm_log.txt 2>&1
 
-# Build
-echo -e "\033[33;1mRunning Coverity Scan Analysis Tool...\033[0m"
-COV_BUILD_OPTIONS=""
-#COV_BUILD_OPTIONS="--return-emit-failures 8 --parse-error-threshold 85"
-RESULTS_DIR="cov-int"
-eval "${COVERITY_SCAN_BUILD_COMMAND_PREPEND}"
-COVERITY_UNSUPPORTED=1 cov-build --dir $RESULTS_DIR $COV_BUILD_OPTIONS $COVERITY_SCAN_BUILD_COMMAND
-cov-import-scm --dir $RESULTS_DIR --scm git --log $RESULTS_DIR/scm_log.txt 2>&1
+    SHA=`git rev-parse --short HEAD`
 
-# Upload results
-echo -e "\033[33;1mTarring Coverity Scan Analysis results...\033[0m"
-RESULTS_ARCHIVE=analysis-results.tgz
-tar czf $RESULTS_ARCHIVE $RESULTS_DIR
-SHA=`git rev-parse --short HEAD`
+    # Upload results
+    echo -e "\033[33;1mUploading Coverity Scan Analysis results...\033[0m"
+    response=$(tar cz $RESULTS_DIR | curl \
+      --silent --write-out "\n%{http_code}\n" \
+      --form project=$COVERITY_SCAN_PROJECT_NAME \
+      --form token=$COVERITY_SCAN_TOKEN \
+      --form email=$COVERITY_SCAN_NOTIFICATION_EMAIL \
+      --form file=@- \
+      --form version=$SHA \
+      --form description="Travis CI build" \
+      $UPLOAD_URL)
+    status_code=$(echo "$response" | sed -n '$p')
+    if [ "$status_code" != "201" ]; then
+      TEXT=$(echo "$response" | sed '$d')
+      echo -e "\033[33;1mCoverity Scan upload failed: $TEXT.\033[0m"
+      exit 1
+    fi
+}
 
-echo -e "\033[33;1mUploading Coverity Scan Analysis results...\033[0m"
-response=$(curl \
-  --silent --write-out "\n%{http_code}\n" \
-  --form project=$COVERITY_SCAN_PROJECT_NAME \
-  --form token=$COVERITY_SCAN_TOKEN \
-  --form email=$COVERITY_SCAN_NOTIFICATION_EMAIL \
-  --form file=@$RESULTS_ARCHIVE \
-  --form version=$SHA \
-  --form description="Travis CI build" \
-  $UPLOAD_URL)
-status_code=$(echo "$response" | sed -n '$p')
-if [ "$status_code" != "201" ]; then
-  TEXT=$(echo "$response" | sed '$d')
-  echo -e "\033[33;1mCoverity Scan upload failed: $TEXT.\033[0m"
-  exit 1
-fi
+case "$MODE" in
+  download )
+    cov_download ;;
+  upload )
+    cov_upload $RESULTS_DIR ;;
+  build )
+    # Check we're permitted to upload, return a build command if we are
+    if cov_check_permitted >& 2; then
+        echo "${TOOL_BASE}/bin/cov-build --dir ${RESULTS_DIR} ${COV_BUILD_OPTIONS}"
+    fi ;;
+esac
